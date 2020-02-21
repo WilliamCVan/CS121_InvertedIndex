8ceeb962b197220f333d497616871b0f2b7a8c83
William Van <wvan77@gmail.com>
2020-02-21 00:05:09 -0800

    -updated to multi-threading across entire list of json files

--- Part1MT.py
+++ Part1MT.py
@@ -5,59 +5,70 @@ import re
 import json
 from bs4 import BeautifulSoup, Comment
 
-def tokenize(directoryPath)-> dict:
-    for files in Path(directoryPath).iterdir():
-        if files.is_file():
-            directoryPath = Path(directoryPath)
-            fullFilePath = directoryPath / files.name
+def getAllFilePaths(directoryPath: str) -> list:
+    listFilePaths = list()
 
-            with open(fullFilePath, 'r') as content_file:
-                textContent = content_file.read()
+    # create list of all subdirectories that we need to process
+    pathParent = Path(directoryPath)
+    directory_list = [(pathParent / dI) for dI in os.listdir(directoryPath) if
+                      Path(Path(directoryPath).joinpath(dI)).is_dir()]
 
-                jsonOBJ = json.loads(textContent)
-                url = jsonOBJ["url"]
-                htmlContent = jsonOBJ["content"]
+    for directory in directory_list:
+        for files in Path(directory).iterdir():
+            if files.is_file():
+                fullFilePath = directory / files.name
+
+                listFilePaths.append(str(fullFilePath))
+
+        return listFilePaths
+
+def tokenize(filePath: str)-> dict:
+        with open(filePath, 'r') as content_file:
+            textContent = content_file.read()
+
+            jsonOBJ = json.loads(textContent)
+            url = jsonOBJ["url"]
+            htmlContent = jsonOBJ["content"]
 
 
-                dictTEMP = dict()
+            dictTEMP = dict()
 
-                # initialize object and pass in html
-                soup = BeautifulSoup(htmlContent, 'html.parser')
+            # initialize object and pass in html
+            soup = BeautifulSoup(htmlContent, 'html.parser')
 
-                # removes comments from text
-                for tag in soup(text=lambda text: isinstance(text, Comment)):
-                    tag.extract()
+            # removes comments from text
+            for tag in soup(text=lambda text: isinstance(text, Comment)):
+                tag.extract()
 
-                # removes javascript and css from text
-                for element in soup.findAll(['script', 'style']):
-                    element.extract()
+            # removes javascript and css from text
+            for element in soup.findAll(['script', 'style']):
+                element.extract()
 
-                # add all tokens found from html response with tags removed
-                varTemp = soup.get_text()
+            # add all tokens found from html response with tags removed
+            varTemp = soup.get_text()
 
-                listTemp = re.split(r"[^a-z0-9']+", varTemp.lower())
-                for word in listTemp:
-                    if(len(word) == 0): #ignore empty strings
-                        continue
+            listTemp = re.split(r"[^a-z0-9']+", varTemp.lower())
+            for word in listTemp:
+                if(len(word) == 0): #ignore empty strings
+                    continue
 
-                    if not word in dictTEMP:
-                        dictTEMP[word] = 1
-                    else:
-                        dictTEMP[word] += 1
+                if not word in dictTEMP:
+                    dictTEMP[word] = 1
+                else:
+                    dictTEMP[word] += 1
 
-                #change the code here to save Postings (tdif, frequency count, linkedList of DocID's, etc)
-                return dictTEMP
+            #change the code here to save Postings (tdif, frequency count, linkedList of DocID's, etc)
+            return dictTEMP
 
 def parseJSONFiles(directoryPath):
-    #create list of all subdirectories that we need to process
-    pathParent = Path(directoryPath)
-    directory_list = [(pathParent / dI) for dI in os.listdir(directoryPath) if Path(Path(directoryPath).joinpath(dI)).is_dir()]
+    listFilePaths = getAllFilePaths(directoryPath)
 
+    # https://stackoverflow.com/questions/2846653/how-can-i-use-threading-in-python
     # Make the Pool of workers
-    pool = ThreadPool(12)
+    pool = ThreadPool(20)
 
     # Each worker get a directory from list above, and begin tokenizing all json files inside
-    results = pool.map(tokenize, directory_list)
+    results = pool.map(tokenize, listFilePaths)
 
     # Close the pool and wait for the work to finish
     pool.close()
