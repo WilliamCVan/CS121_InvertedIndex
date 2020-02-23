c0b108369c185030cc0103aef90f29a233faf81c
William Van <wvan77@gmail.com>
2020-02-22 17:41:48 -0800

    -updated code for MT

--- Part1MT.py
+++ Part1MT.py
@@ -5,63 +5,110 @@ import re
 import json
 from bs4 import BeautifulSoup, Comment
 
+class Posting:
+    def __init__(self, docID, freqCount):
+        self.docID = docID
+        self.freqCount = freqCount
+
+    def incFreq(self):
+        self.freqCount += 1
+
+    def show(self):
+        return [self.docID, self.freqCount]
+
 def getAllFilePaths(directoryPath: str) -> list:
     listFilePaths = list()
+    hashTableIDToUrl = dict()
 
     # create list of all subdirectories that we need to process
     pathParent = Path(directoryPath)
     directory_list = [(pathParent / dI) for dI in os.listdir(directoryPath) if
                       Path(Path(directoryPath).joinpath(dI)).is_dir()]
 
+    iDocID = 0
+
     for directory in directory_list:
+        #print(str(directory))
         for files in Path(directory).iterdir():
             if files.is_file():
                 fullFilePath = directory / files.name
+                listFilePaths.append([iDocID, str(fullFilePath)])
+                hashTableIDToUrl[iDocID] = str(fullFilePath)
+                iDocID += 1
 
-                listFilePaths.append(str(fullFilePath))
+    # write json file that maps the docID's to file path urls
+    writeHashTableToDisk(hashTableIDToUrl)
 
-        return listFilePaths
-
-def tokenize(filePath: str)-> dict:
-        with open(filePath, 'r') as content_file:
-            textContent = content_file.read()
-
-            jsonOBJ = json.loads(textContent)
-            url = jsonOBJ["url"]
-            htmlContent = jsonOBJ["content"]
+    return listFilePaths
 
 
-            dictTEMP = dict()
+def tokenize(fileItem: list) -> dict:
+    filePath = fileItem[1]
+    docID = fileItem[0]
 
-            # initialize object and pass in html
-            soup = BeautifulSoup(htmlContent, 'html.parser')
+    with open(filePath, 'r') as content_file:
+        textContent = content_file.read()
 
-            # removes comments from text
-            for tag in soup(text=lambda text: isinstance(text, Comment)):
-                tag.extract()
+        jsonOBJ = json.loads(textContent)
+        url = jsonOBJ["url"]
+        htmlContent = jsonOBJ["content"]
 
-            # removes javascript and css from text
-            for element in soup.findAll(['script', 'style']):
-                element.extract()
+        tokenDict = dict()
 
-            # add all tokens found from html response with tags removed
-            varTemp = soup.get_text()
+        # initialize object and pass in html
+        soup = BeautifulSoup(htmlContent, 'html.parser')
 
-            listTemp = re.split(r"[^a-z0-9']+", varTemp.lower())
-            for word in listTemp:
-                if(len(word) == 0): #ignore empty strings
-                    continue
+        # removes comments from text
+        for tag in soup(text=lambda text: isinstance(text, Comment)):
+            tag.extract()
 
-                if not word in dictTEMP:
-                    dictTEMP[word] = 1
-                else:
-                    dictTEMP[word] += 1
+        # removes javascript and css from text
+        for element in soup.findAll(['script', 'style']):
+            element.extract()
 
-            #change the code here to save Postings (tdif, frequency count, linkedList of DocID's, etc)
-            return dictTEMP
+        # add all tokens found from html response with tags removed
+        varTemp = soup.get_text()
 
-def parseJSONFiles(directoryPath):
+        listTemp = re.split(r"[^a-z0-9']+", varTemp.lower())
+        for word in listTemp:
+            if (len(word) == 0):  # ignore empty strings
+                continue
+            if word in tokenDict:
+                tokenDict.get(word).incFreq()
+            else:
+                tokenDict[word] = Posting(docID, 1)
+
+        # write partial indexes to text files ("store on disk")
+        buildPartialIndex(tokenDict)
+        # merge later
+
+        # # change the code here to save Postings (tdif, frequency count, linkedList of DocID's, etc)
+        # return tokenDict
+
+
+# write json file that maps the docID's to file path urls
+def writeHashTableToDisk(hashtable: dict) -> None:
+    if not os.path.exists("partial_indexes"):
+        Path("partial_indexes").mkdir()
+    with open(os.path.join("partial_indexes", "hashtable.txt"), "w+") as hash:
+        hash.write(json.dumps(hashtable))
+
+
+def buildPartialIndex(tokenDict):
+    # make subdirectory for partial indexes
+    if not os.path.exists("partial_indexes"):
+        Path("partial_indexes").mkdir()
+
+    # write text files to store inndexing data for merging later
+    with open(os.path.join("partial_indexes", "index.txt"), "a") as partialIndex:
+        for key in sorted(tokenDict):
+            partialIndex.write(key + " : " + str(tokenDict.get(key).show()) + '\n');
+
+
+def parseJSONFiles(directoryPath: str) -> int:
     listFilePaths = getAllFilePaths(directoryPath)
+
+    DOC_NUM = len(listFilePaths)    #number of files we will tokenize
 
     # https://stackoverflow.com/questions/2846653/how-can-i-use-threading-in-python
     # Make the Pool of workers
@@ -70,11 +117,22 @@ def parseJSONFiles(directoryPath):
     # Each worker get a directory from list above, and begin tokenizing all json files inside
     results = pool.map(tokenize, listFilePaths)
 
+    # for key in results[0]:
+    # print(key, " : ", results[0].get(key).show())
+
     # Close the pool and wait for the work to finish
     pool.close()
     pool.join()
 
+    return
 
 if __name__ == '__main__':
+    # Aljon
+    #folderPath = "C:\\Users\\aljon\\Documents\\CS121_InvertedIndex\\DEV"
+
+    # William
     folderPath = "C:\\Anaconda3\\envs\\Projects\\DEV"
-    parseJSONFiles(folderPath)
+
+    iDocsCount = parseJSONFiles(folderPath)
+    print("Number of docs = ", iDocsCount)
+    print("-----DONE!-----")
