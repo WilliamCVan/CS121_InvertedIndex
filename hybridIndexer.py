a0d38b82ea0c354169745ab0507b7be5952a90f2
Jerome <arkserius@yahoo.com>
2020-03-08 15:40:59 -0700

    updated hybridstem

--- hybridIndexer.py
+++ hybridIndexer.py
@@ -6,115 +6,213 @@ import json
 from bs4 import BeautifulSoup, Comment
 import string
 import nltk
-
-#nltk.download('punkt')
+# nltk.download('punkt')
 from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+import math
+import utils as util
+
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet as wn
 from nltk import pos_tag
-import math
 from collections import defaultdict
-from functools import lru_cache
-
 
 tag_map = defaultdict(lambda: wn.NOUN)
 tag_map['J'] = wn.ADJ
 tag_map['V'] = wn.VERB
 tag_map['R'] = wn.ADV
 
+
 # Data Structures
+
+# 'Posting' objects store the info we later put into each token.json file in our index
+# Initially, each token and Posting we find in each document goes into index.txt
+# Postings contain docID (where token was found), RAW frequency (in the document of docID), and tag (from HTML content)
 class Posting:
-    def __init__(self, docID, freqCount):
-        self.docID = docID
-        self.freqCount = freqCount
+    def __init__(self, docID, frequency, tag):
+        self.docID = int(docID)
+        self.frequency = int(frequency)
+        self.tag = str(tag)
 
     def incFreq(self):
-        self.freqCount += 1
+        self.frequency += 1
 
-    def show(self):
-        return [self.docID, self.freqCount]
+    def showData(self):
+        return [self.docID, self.frequency, self.tag]
 
 
 # Main Functions (aka functions called in __main__)
 
 # Creates subdirectories for each integer and letter.
 def createPartialIndexes() -> None:
-    if not Path("partial_indexes").exists():
-        Path("partial_indexes").mkdir()
-
+    if not Path("index").exists():
+        Path("index").mkdir()
     for char in list(string.ascii_lowercase):
-        pathFull = Path("partial_indexes") / char
+        pathFull = Path("index") / char
         if not pathFull.exists():
             pathFull.mkdir()
-
     for num in list("0123456789"):
-        pathFull = Path("partial_indexes") / num
+        pathFull = Path("index") / num
         if not pathFull.exists():
             pathFull.mkdir()
 
 
-def parseJSONFiles(directoryPath: str) -> int:
-    listFilePaths = getAllFilePaths(directoryPath)
+# Uses multithreading, tokenizes every document in the "DEV" corpus
+def parseJSONFiles(directoryPath: str) -> None:
+    filePathsList = getAllFilePaths(directoryPath)  # 55K+ json files to process
 
-    # DOC_NUM = len(listFilePaths)    #number of files we will tokenize
+    #for filePath in filePathsList:
+        #tokenize(filePath)
 
     # https://stackoverflow.com/questions/2846653/how-can-i-use-threading-in-python
     # Make the Pool of workers
     pool = Pool(processes=20)
     # Each worker get a directory from list above, and begin tokenizing all json files inside
-    pool.map(tokenize, listFilePaths)
+    pool.map(tokenize, filePathsList)
     # Close the pool and wait for the work to finish
     pool.close()
     pool.join()
 
+def mergeHelper(line):
+    try:
+        # Convert raw text in this line into Posting data
+        posting = line.split(" : ")
+        token = posting[0].replace("'", "")
+        postingList = posting[1].replace("[", "").replace("]", "").replace("\n", "").split(", ")
 
-# Reads index.txt line by line, then merges the frequencies of each token.
-# Finally, it collects a list of DocIDs into a list for each token.
+        # Collect data about the token from this line
+        newDocID = int(postingList[0])
+        newFreq = int(postingList[1])
+        newTag = str(postingList[2].strip("'"))
+
+        # Create a new filename and filepath for this token
+        firstLetter = token[0:1]
+        filePathFull = Path("index") / firstLetter / (token + ".json")
+
+        # If file already exists, then we read it and update it
+        if filePathFull.is_file():
+            with open(filePathFull, "r+") as posting:
+                # Add to the existing data and save updated values back to json
+                posting.seek(0)  # reset to beginning of file to overwrite
+                data = posting.read()
+                jsonObj = json.loads(data)
+                jsonObj["freq"] += newFreq
+                jsonObj["docIDList"] = sorted(jsonObj["docIDList"].append([newDocID, newTag]))
+                posting.write(json.dumps(jsonObj))
+
+        else:
+            # Otherwise, write it from scratch
+            jsonObj = {"freq": newFreq, "listDocIDs": [[newDocID, newTag]]}
+            with open(filePathFull, "w+") as posting:
+                posting.write(json.dumps(jsonObj))
+    except:
+        pass
+
+def multiMergeTokens():
+    indexTxt = open(os.path.join("index", "index.txt"), 'r')
+    lineBuffer = []
+
+    for line in indexTxt:
+        lineBuffer.append(line)
+
+        if len(lineBuffer) >= 10000:
+            pool = Pool(processes=20)
+            # Each worker get a directory from list above, and begin tokenizing all json files inside
+            pool.map(mergeHelper, lineBuffer)
+            # Close the pool and wait for the work to finish
+            pool.close()
+            pool.join()
+
+            lineBuffer.clear()
+
+    indexTxt.close()
+
+
+
+# Reads index.txt line by line, then sums the frequencies of each token.
+# Next, it collects a list of DocIDs into a list for each token.
+# Finally, it adds the tag the token had originally
 def mergeTokens():
-    indexTxt = open(os.path.join("partial_indexes", "index.txt"), 'r')
+    indexTxt = open(os.path.join("index", "index.txt"), 'r')
 
     for line in indexTxt:
         try:
-            arrPosting = line.split(" : ")
-            wordToken = arrPosting[0].replace("'", "")
-            firstLetter = wordToken[0:1]
+            # Convert raw text in this line into Posting data
+            posting = line.split(" : ")
+            token = posting[0].replace("'", "")
+            postingList = posting[1].replace("[", "").replace("]", "").replace("\n", "").split(", ")
 
-            templist = arrPosting[1].replace("[", "").replace("]", "").replace("\n", "").split(", ")
-            docID = int(templist[0])
-            frequency = templist[1]
+            # Collect data about the token from this line
+            newDocID = int(postingList[0])
+            newFreq = int(postingList[1])
+            newTag = str(postingList[2].strip("'"))
 
-            filePathFull = Path("partial_indexes") / firstLetter / (wordToken + ".json")
+            # Create a new filename and filepath for this token
+            firstLetter = token[0:1]
+            filePathFull = Path("index") / firstLetter / (token + ".json")
 
-            # If file exists, then we read it out as json
+            # If file already exists, then we read it and update it
             if filePathFull.is_file():
                 with open(filePathFull, "r+") as posting:
+                    # Add to the existing data and save updated values back to json
+                    posting.seek(0)  # reset to beginning of file to overwrite
                     data = posting.read()
                     jsonObj = json.loads(data)
-                    jsonCount = jsonObj["freq"]
-                    jsonListPostings = jsonObj["listDocIDs"]
-
-                    newFreq = jsonCount + int(frequency)
-                    jsonListPostings.append(docID)
-
-                    # save updated values back to json
-                    posting.seek(0)  # reset to beginning of file to overwrite
-                    jsonObj["freq"] = newFreq
-                    jsonObj["listDocIDs"] = sorted(jsonListPostings)
+                    jsonObj["freq"] += newFreq
+                    jsonObj["docIDList"] = sorted(jsonObj["docIDList"].append([newDocID, newTag]))
                     posting.write(json.dumps(jsonObj))
-            else:
-                jsonObj = {"freq": 0, "listDocIDs": []}
-                newFreq = jsonObj["freq"] + int(frequency)
-                newListID = [int(docID)]
 
-                # save updated values back to json
-                jsonObj["freq"] = newFreq
-                jsonObj["listDocIDs"] = newListID
+            else:
+                # Otherwise, write it from scratch
+                jsonObj = {"freq": newFreq, "listDocIDs": [[newDocID, newTag]]}
                 with open(filePathFull, "w+") as posting:
                     posting.write(json.dumps(jsonObj))
         except:
             continue
 
-    indexTxt.close()
+
+# Calculate TF-IDF scores for a given (token:document) in index.txt and 'DEV' corpus
+def calculateTFIDF(queryList, unrankedDocList, folderPath):
+    indexFile = open(os.path.join(folderPath, "index.txt"), 'r')
+    hashtableFile = open(os.path.join(folderPath, "hashtable.txt"), 'r')
+    hashtable = json.load(hashtableFile)
+    N = 13518180  # N = len(indexTxt.readlines())
+
+    # Create dict of {key=docURL : value=TF-IDF score}
+    tfidfDict = dict.fromkeys(unrankedDocList, 0)
+    for token in queryList:
+        with open(os.path.join(folderPath, queryList[0][0], f"{queryList[0]}.json"), 'r') as jsonFile:
+            tokenInfo = json.load(jsonFile)
+            # Get RAW TF (Token Frequency in all docs) from token's json file in index
+            RawTF = tokenInfo["freq"]
+            # Get N (Number of docs the token is in) from token's json file in index (not right??)
+            # N = len(tokenInfo["listDocIDs"])
+
+        # Create a "temporary" dict of {key=docURL : value=frequency of token in this doc}
+        docFreqDict = dict.fromkeys(unrankedDocList, 0)
+        # Return to top of file, if not already there
+        indexFile.seek(0)
+        for line in indexFile.readlines():
+            # Get the frequency of this token for this specific document
+            # Have to go line-by-line in index.txt to find them, but only once per token
+            line = str(line)
+            if line.startswith(token):
+                # Parses Posting from index.txt -> [0] = DocID, [1] = freq for this doc
+                indexItems = re.findall(r"\[.*\]", line)[0].strip("][").split(', ')
+                currentDocURL = hashtable[indexItems[0]]
+                # If current DocID is in our dictionary, add to its freq total
+                if currentDocURL in docFreqDict:
+                    docFreqDict[currentDocURL] += int(indexItems[1])
+
+        # Calculate TF-IDF score for this document
+        for key in tfidfDict:
+            if docFreqDict[key] == 0:
+                tfidfDict[key] = 0
+            else:
+                tfidfDict[key] = (1 + math.log(RawTF)) * math.log(N / docFreqDict[key])
+
+    # Note: tfidfDict considers the entire query (Works similar to BoolAnd search)
+    return tfidfDict
 
 
 ### Helper Functions (aka functions called by other functions) ###
@@ -138,86 +236,105 @@ def getAllFilePaths(directoryPath: str) -> list:
                 hashTable[index] = str(fullFilePath)
 
     # Writes "hashtable" file that maps the docIDs to filepaths of those documents.
-    with open(os.path.join("partial_indexes", "hashtable.txt"), "w+") as hash:
+    with open(os.path.join("index", "hashtable.txt"), "w+") as hash:
         hash.write(json.dumps(hashTable))
 
     return filePathsList
 
 
+# Tokenizes and collects data from one json file from the "DEV" corpus, returns a dict of (token : Posting)
 def tokenize(fileItem: list) -> dict:
+    ps = PorterStemmer().stem
+    wnl = WordNetLemmatizer()
+    lem = wnl.lemmatize
+    lemmaCache = dict()
+
+    tokenDict = dict()
     filePath = fileItem[1]
     docID = int(fileItem[0])
 
     with open(filePath, 'r') as content_file:
         textContent = content_file.read()
-
         jsonOBJ = json.loads(textContent)
         htmlContent = jsonOBJ["content"]
 
-        tokenDict = dict()
-
-        # initialize object and pass in html
+        # initialize BeautifulSoup object and pass in html content
         soup = BeautifulSoup(htmlContent, 'html.parser')
 
-        # removes comments from text
+        # Deletes HTML comments, javascript, and css from text
         for tag in soup(text=lambda text: isinstance(text, Comment)):
             tag.extract()
-
-        # removes javascript and css from text
         for element in soup.findAll(['script', 'style']):
             element.extract()
 
+        # Collect all words found from html response WITH TAGS IN A TUPLE WITH EACH WORD ('word', 'tag')
+        # Tags below are in order of importance/weight
+        tagNamesList = ['title', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'strong', 'b', 'a', 'p', 'span', 'div']
+        tagsTextList = []
+        for tag in tagNamesList:
+            tagsTextList.append(soup.find_all(tag))
+
+        ##### REDIS ONLY START #####
+        # return if html text has identical hash
         # add all tokens found from html response with tags removed
-        varTemp = soup.get_text()
+        # varTemp = soup.get_text()
+        # if util.isHashSame(varTemp):
+        # return dict()
 
-        listTemp = re.split(r"[^a-z0-9']+", varTemp.lower())
-        ps = PorterStemmer().stem
-        wnl = WordNetLemmatizer()
-        lem = lru_cache(maxsize=50000)(wnl.lemmatize)
+        # add unique url to redis
+        # urlContent = jsonOBJ["url"]
+        # util.addUniqueURL(docID, urlContent)
+        ##### REDIS ONLY END #####
 
-        for word in listTemp:
-            if (len(word) == 0):  # ignore empty strings
-                continue
-            if (len(word) > 30):  # ignore words like ivborw0kggoaaaansuheugaaabaaaaaqcamaaaaolq9taaaaw1bmveuaaaacagiahb0bhb0bhr0ahb4chh8dhx8eicifisiukt4djzankywplcwhltkfpl8nn0clpvm9qumvvxu8wnvbrezesepkyxvwzxbpbnjqb3jtcxruc3vvdxhzdnhyehtefjvdf5xtjkv
-                continue
-            if (word[0] == "'"):  # ignore words that start with '
-                continue
-            if (word[:3] == "tct"): # ignore garbage like tctgactgtctgg
-                continue
-            if (len(word) == 1 and word.isalpha()):  # ignore single characters
-                continue
-            if any(char.isdigit() for char in word):
-                try:
-                    word = str(int(word))  # get rid of numbers starting with 0 such as 00 or 01 and just stores 0 or 1
-                except ValueError:
-                    pass
+        taggedTextDict = dict()
+        for i, tagSubList in enumerate(tagsTextList):
+            taggedTextDict[tagNamesList[i]] = list()
+            for phrase in tagSubList:
+                for word in re.split(r"[^a-z0-9']+", phrase.get_text().lower()):
+                    taggedTextDict.get(tagNamesList[i]).append(word)
 
-            elif len(word) > 2 and word not in tokenDict:# > 2 So we dont have to find the lemma of as, in, to etc and waste time.
-                pos = tag_map[pos_tag((word,))[0][1][0]]
-                word = lem(word,pos)#ps.stem(word)  # run Porter stemmer on token
+        # Store words as tokens in tokenDict, ignore words that are bad
+        for tag, wordList in taggedTextDict.items():
+            for word in wordList:
+                if (len(word) == 0):  # ignore empty strings
+                    continue
+                if (len(word) > 30 and tag != 'a'):  # ignore words like ivborw0kggoaaaansuheugaaabaaaaaqcamaaaaolq9taaaaw1bmveuaaaacagiahb0bhb0bhr0ahb4chh8dhx8eicifisiukt4djzankywplcwhltkfpl8nn0clpvm9qumvvxu8wnvbrezesepkyxvwzxbpbnjqb3jtcxruc3vvdxhzdnhyehtefjvdf5xtjkv
+                    continue  # But accept any URLs that may be large
+                if (word[0] == "'"):  # ignore words that start with '
+                    continue
+                if (len(word) == 1 and word.isalpha()):  # ignore single characters
+                    continue
+                if not any(char.isdigit() for char in word) and len(word) > 2 and word not in lemmaCache:
+                    # Lemmatization of a word with a number is usually itself.
+                    # lemmatization of in, on, as, is usually itself.
+                    # Checking for the above and if word is not already cached saves time.
+                    pos = tag_map[pos_tag((word,))[0][1][0]]
+                    lemWord = lem(word, pos)
+                    if word[-2:] == "ly" or word[-4:] == "ness" or word[-3:] == "ish":  # Catches any ly, ness, or ish that lemmatize doesnt catch. Words are less accurate, but cuts off extraneous words.
+                        lemWord = ps(word)
+                    lemmaCache[word] = lemWord
+                else:
+                    lemmaCache[word] = word #the lemma of the word is itself
 
-                if word[-2:] == "ly" or word[-4:] == "ness" or word[-3:] == "ish":# Catches any ly, ness, or ish that lemmatize doesnt catch. Words are less accurate, but cuts off extraneous words.
-                    word = ps(word)
+                if lemmaCache[word] in tokenDict:
+                    tokenDict.get(lemmaCache[word]).incFreq()
+                else:
+                    tokenDict[lemmaCache[word]] = Posting(docID, 1, tag)
 
-            if word in tokenDict:
-                tokenDict.get(word).incFreq()
-            else:
-                tokenDict[word] = Posting(docID, 1)
+                if len(lemmaCache) > 5000000:#Save up to 5million tokens, and then clear to prevent too much memory error
+                    lemmaCache.clear()
 
-        # write partial indexes to text files ("store on disk")
-        content_file.close()#Got an error opening index.txt so closed content_file to be safe.
+        # Write tokens and their Postings to a text file ("store on disk")
         buildIndex(tokenDict)
         # buildMultipleIndexes(tokenDict)
-        # merge later
-        # change the code here to save Postings (tdif, frequency count, linkedList of DocID's, etc)
         return tokenDict
 
 
-def buildIndex(tokenDict):
-    # write text files to store indexing data for merging later
-    with open(os.path.join("partial_indexes", "index.txt"), "a") as partialIndex:
+def buildIndex(tokenDict: dict) -> None:
+    # Write index.txt to store indexing data for merging later
+    with open(os.path.join("index", "index.txt"), "a") as partialIndex:
         for key in sorted(tokenDict):
-            partialIndex.write(key + " : " + str(tokenDict.get(key).show()) + '\n')
+            partialIndex.write(key + " : " + str(tokenDict.get(key).showData()) + '\n')
 
 
 if __name__ == '__main__':
@@ -227,24 +344,28 @@ if __name__ == '__main__':
     # folderPath = "C:\\Users\\aljon\\Documents\\CS_121\\Assignment_3\\DEV"
 
     # William
-    # folderPath = "C:\\Anaconda3\\envs\\Projects\\DEV"
+    # folderPath = "C:\\Anaconda3\\envs\\Projects\\developer\\DEV"
 
     # Jerome
-    #folderPath = "C:\\Users\\arkse\\Desktop\\CS121_InvertedIndex\\DEV"
+    folderPath = "C:\\Users\\arkse\\Desktop\\CS121_InvertedIndex\\DEV"
 
     # Art - windows
     # folderPath = "C:\\Users\\aghar\\Downloads\\DEV"
     # Art - linux
     # folderPath = "/home/anon/Downloads/DEV"
 
-    #print("Creating partial index folders...")
-    #createPartialIndexes()
-    #print("Parsing JSON files, creating index.txt...")
+    print("Creating partial index folders...")
+    createPartialIndexes()
+
+    #print("Parsing 'DEV' JSON files, building index.txt...")
     #parseJSONFiles(folderPath)
-    # print("Merge partial indexes")
-    # mergeIndexes()
-    #print("Merging tokens, organizing files into partial index folders")
-    #mergeTokens()
+
+    print("Merging tokens from index.txt, storing token.JSON files into index...")
+    multiMergeTokens()
+
+    # Note: Calculating TF-IDF has to be done AFTER mergeTokens()
+    # Because it needs the full frequency for each token
     # print("Calculating TF-IDF scores for each token...")
     # calculateTFIDF()
+
     print("-----DONE!-----")
